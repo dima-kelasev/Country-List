7ea89586b8bfabf76b7193828aadbb5da25e944e
Dima Kelasev <dimakelaseff@gmail.com>
2022-04-29 14:30:21 +0400

    add condition for mobile version

--- src/components/LineChart/index.tsx
+++ src/components/LineChart/index.tsx
@@ -31,38 +31,45 @@ export function LineChart({ capital }: LineChartProps) {
   return (
     <>
       {weather?.current.condition.icon && <CurrentForecast data={weather} />}
-
-      {hour ? (
-        <div className="diagram_box">
-          <div className="data_box data_time_box">
-            {hour?.map((hour, i) => (
-              <p key={i} className="data time">
-                {hour.time.slice(11, 13)}
-              </p>
-            ))}
+      <div className="line_chart">
+        {hour ? (
+          <div className="diagram_box">
+            <div className="data_box data_time_box">
+              {hour?.map((hour, i) => (
+                <p key={i} className="data time">
+                  {hour.time.slice(11, 13)}
+                </p>
+              ))}
+            </div>
+            <p className="description">Hour's</p>
+            <svg x="0px" y="0px" viewBox="0 0 1000 2" height="100" width="100%">
+              <polyline
+                points={polyline}
+                style={{ background: "white" }}
+                stroke="#fff"
+                strokeWidth="5"
+              />
+              {arrayX.map((coordX, i) => (
+                <circle
+                  key={i}
+                  cx={coordX}
+                  r={6}
+                  cy={arrayY?.[i]}
+                  fill="#fff"
+                />
+              ))}
+            </svg>
+            <div className="data_box">
+              {hour?.map((hour, i) => (
+                <p key={i} className="data">{`${hour.temp_c}°`}</p>
+              ))}
+            </div>
+            <p className="description">temperature °</p>
           </div>
-          <p className="description">Hour's</p>
-          <svg x="0px" y="0px" viewBox="0 0 1000 2" height="100" width="100%">
-            <polyline
-              points={polyline}
-              style={{ background: "white" }}
-              stroke="#fff"
-              strokeWidth="5"
-            />
-            {arrayX.map((coordX, i) => (
-              <circle key={i} cx={coordX} r={6} cy={arrayY?.[i]} fill="#fff" />
-            ))}
-          </svg>
-          <div className="data_box">
-            {hour?.map((hour, i) => (
-              <p key={i} className="data">{`${hour.temp_c}°`}</p>
-            ))}
-          </div>
-          <p className="description">temperature °</p>
-        </div>
-      ) : (
-        <Spinner />
-      )}
+        ) : (
+          <Spinner />
+        )}
+      </div>
     </>
   );
 }
